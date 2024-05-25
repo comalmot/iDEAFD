f073b7f328cfe6b6f0ad108eb88bc095558c7b03
Gunseung Jin <gunseungjin@gmail.com>
2024-05-26 04:27:38 +0900

    Add Auto Debug Mode Module

---
+++ jtag_debugmode.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import subprocess
+import time
+import sys
+
+class JTAGDebugMode:
+	def __init__(self, devCode, current_dir):		
+		self.devCode = devCode
+		self.current_dir = current_dir
+		self.commands = [
+			['python', 'ipwndfu', '-p', '--demote', '--disable-wdt'],
+			['openocd', '-f', 'Modules/bonobo-configs/t8010.cfg'],
+			['ssh', '127.0.0.1'],
+		]
+		
+		print("Ready DFU Mode. we will wait 10 seconds..")
+		time.sleep(10)
+	
+		for command in self.commands:
+			if "ipwndfu" in command:
+				script = f'tell application "Terminal" to do script "cd {self.current_dir}/Modules/ipwndfu-nowdt/ && {command[0]} {" ".join(command[1:])}"'			
+			else:
+				script = f'tell application "Terminal" to do script "cd {self.current_dir} && {command[0]} {" ".join(command[1:])}"'
+			print("[+] Debug : " + script)
+			subprocess.run(["osascript", "-e", script])	
+
+		# Invoke GDB
+		subprocess.run(['osascript', '-e', '''
+    			tell application "Terminal"
+        		activate
+        		do script "gdb -q" in front window
+        		delay 1
+        		do script "target remote :3333" in front window
+        		delay 1
+    			end tell
+		'''])
